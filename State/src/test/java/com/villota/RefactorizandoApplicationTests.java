7ba6de3b04fc88e14c53e32d49237ad194918498
NicolasV7 <159790870+NicolasV7@users.noreply.github.com>
2024-05-29 20:46:15 -0400

    State and Strategy

---
+++ State/src/test/java/com/villota/RefactorizandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.villota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class villotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
